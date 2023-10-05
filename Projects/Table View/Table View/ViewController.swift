b08ba54f0bba0f69408560bf352cd27379623bbc
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-10-06 00:33:17 +0300

    Table View Project: initial commit for table view project

---
+++ Projects/Table View/Table View/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  File name: ViewController.swift
+//  Project name: Table View
+//  Workspace name: UIKit-Samples
+//
+//  Created by: nothing-to-add on 06/10/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
