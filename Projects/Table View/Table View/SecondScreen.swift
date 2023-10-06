faf78c78bfc05fd50749e752601ac0115179d83f
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-10-06 16:12:04 +0300

    Table View Project: implementation of navigation from one screen to another

---
+++ Projects/Table View/Table View/SecondScreen.swift
@@ -0,0 +1,20 @@
+//
+//  File name: SecondScreen.swift
+//  Project name: Table View
+//  Workspace name: UIKit-Samples
+//
+//  Created by: nothing-to-add on 06/10/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+}
