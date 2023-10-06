c4e1c6f268e2c5e4a8d21e418a18ab76496eb11f
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-10-06 17:12:32 +0300

    Table View Project: implementation of navigation to Table view screen

---
+++ Projects/Table View/Table View/TableViewScreen.swift
@@ -0,0 +1,21 @@
+//
+//  File name: TableViewScreen.swift
+//  Project name: Table View
+//  Workspace name: UIKit-Samples
+//
+//  Created by: nothing-to-add on 06/10/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import UIKit
+
+class TableViewScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+        
+        title = "Table View Screen"
+    }
+}
