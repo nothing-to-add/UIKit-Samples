5ed8cba7e318bbea20af9baaf6b21472d579e888
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-10-06 22:56:08 +0300

    Table View Project: adding images and implementing constants for images names

---
+++ Projects/Table View/Table View/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  File name: Constants.swift
+//  Project name: Table View
+//  Workspace name: UIKit-Samples
+//
+//  Created by: nothing-to-add on 06/10/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import UIKit
+
+struct Images {
+    static let drone = UIImage(named: "drone")!
+    static let mario = UIImage(named: "mario")!
+    static let trade = UIImage(named: "trade")!
+    static let camera = UIImage(named: "camera")!
+    static let gameRemote = UIImage(named: "game-remote")!
+    static let tapes = UIImage(named: "tapes")!
+    static let motherboard = UIImage(named: "motherboard")!
+    static let cat  = UIImage(named: "cat")!
+    static let vrSet  = UIImage(named: "vr-set")!
+    static let island  = UIImage(named: "island")!
+    static let gameBoy  = UIImage(named: "game-boy")!
+}
